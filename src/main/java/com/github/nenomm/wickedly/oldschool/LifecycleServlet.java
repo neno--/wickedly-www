d3325ee875d865c2ed782dd8217ea5dc281ae078
nenad <neno.nenadovic@gmail.com>
2017-05-03 08:42:33 +0200

    add logging for servlet lifecycle methods

---
+++ src/main/java/com/github/nenomm/wickedly/oldschool/LifecycleServlet.java
@@ -0,0 +1,37 @@
+package com.github.nenomm.wickedly.oldschool;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LifecycleServlet extends HttpServlet {
+	static final Logger log = LoggerFactory.getLogger(LifecycleServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		log.info("in doGet method");
+		StringBuilder text = new StringBuilder("lifecycle servlet");
+		resp.getOutputStream().write(text.toString().getBytes());
+	}
+
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		log.info("In init method, calling super");
+		super.init(servletConfig);
+		log.info("In init method, finished with super");
+	}
+
+	@Override
+	public void destroy() {
+		log.info("In destroy method");
+	}
+}
