2901a370d3b93d92187822832768b2ad72779296
nenad <neno.nenadovic@gmail.com>
2017-05-09 22:51:20 +0200

    different mapping types

--- mvc-xml/src/main/java/com/github/nenomm/wickedly/mvcxml/controller/HelloWorldController.java
+++ mvc-xml/src/main/java/com/github/nenomm/wickedly/mvcxml/controller/HelloWorldController.java
@@ -1,25 +1,37 @@
 package com.github.nenomm.wickedly.mvcxml.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
 	static final Logger log = LoggerFactory.getLogger(HelloWorldController.class);
 
-	@RequestMapping("/helloWorld")
-	public String helloWorld(Model model) {
+	@RequestMapping(path = "/helloWorld", params = "a=trolo")
+	public String helloWorld(Model model, HttpServletRequest req, HttpServletResponse resp) {
 		model.addAttribute("message", "Hello World!");
 		return "springView";
 	}
 
-	@RequestMapping(path = "/{day}", method = RequestMethod.GET)
+	@RequestMapping(path = "/day/{day}", method = RequestMethod.GET)
 	public void bla(@PathVariable String day) {
 		log.info("got pathVar: {}", day);
+	}
+
+	@RequestMapping(path = "/pets/{petId}")
+	@ResponseBody
+	public String findPet(@PathVariable String petId, Model model) {
+		log.info("Parameter myParam has value of myValue.");
+		return petId;
 	}
 }
